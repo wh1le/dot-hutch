f8896bb1a317a2b01f94ddb1e3ebe40b3e5bcad3
nmiloserdov09@gmail.com <nmiloserdov09@gmail.com>
2025-09-10 10:44:48 +0100

    hide lazy, improve script delivery

--- bin/deliver.sh
+++ bin/deliver.sh
@@ -3,6 +3,7 @@ echo "Linking files ..."
 ln -s ~/code/dot-files/files/.config/nvim ~/.config/nvim
 ln -s ~/code/dot-files/files/.config/wezterm/.wezterm.lua ~/.config/wezterm/wezterm.lua
 ln -s ~/code/dot-files/files/.zshrc ~/.zshrc
+ln -s ~/code/dot-files/files/.zsh ~/.zsh
 ln -s ~/code/dot-files/files/.dir_colors_eink ~/.dir_colors_eink
 ln -s ~/code/dot-files/files/.tmux.conf ~/.tmux.conf
 ln -s ~/code/dot-files/files/.tmux ~/.tmux
