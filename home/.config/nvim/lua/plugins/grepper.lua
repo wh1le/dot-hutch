32258d1b8cd59a9c02bda2345b520a9259b4c096
Nikita Miloserdov <nmiloserdov09@gmail.com>
2025-11-29 13:17:24 +0000

    use grepper instead of specture global search

---
+++ home/.config/nvim/lua/plugins/grepper.lua
@@ -0,0 +1,29 @@
+return {
+	"mhinz/vim-grepper",
+	lazy = false,
+	config = function()
+		vim.g.grepper = {
+			tools = { "rg", "vimgrep", "grep" }, -- Order of preference
+			searchreg = 1, -- Use last search register
+			highlight = 1, -- Highlight matches
+			quickfix = 1, -- Use quickfix list
+			open = 1, -- Auto-open quickfix
+			switch = 1, -- Switch to quickfix window
+			jump = 0, -- Don't auto-jump to first match
+			prompt = 1, -- Show search prompt
+		}
+
+		-- Keybindings
+		vim.keymap.set("n", "<leader>g", "<cmd>Grepper<CR>", { desc = "Grepper search" })
+		vim.keymap.set("n", "<leader>sw", "<cmd>Grepper -cword -noprompt<CR>", { desc = "Search word under cursor" })
+		vim.keymap.set("n", "<leader>sb", "<cmd>Grepper -buffers<CR>", { desc = "Search in buffers" })
+
+		-- Visual mode: search selected text
+		vim.keymap.set("x", "sw", "<plug>(GrepperOperator)", { desc = "Grepper operator" })
+	end,
+	keys = {
+		{ "<leader>g", desc = "Grepper search" },
+		{ "<leader>g", desc = "Search word" },
+		{ "<leader>sb", desc = "Search buffers" },
+	},
+}
