01029c94123cbfc78443869fb0d48a3e41fa3b87
Nikita Miloserdov <nmiloserdov09@gmail.com>
2025-12-12 14:37:45 +0000

    add config

--- hosts/modules/software/syncthing.nix
+++ hosts/modules/software/syncthing.nix
@@ -1,33 +1,30 @@
 { ... }: {
   services.syncthing = {
     enable = true;
-    user = "syncthing";
-    group = "syncthing";
+    user = "wh1le";
+    group = "users";
+    dataDir = "/home/wh1le/.syncthing";
 
     settings = {
-      folders = {
-        "shared-music-playlists" = {
-          path = "/srv/syncthing";
-          devices = [ ];
-        };
+      devices = {
+        "thinkpad" = { id = "VD4AZ5X-CNJNUPW-EIYE7ET-3FBPEA5-IXMRR3T-POX64IF-XR3E5AJ-LSE3UQZ"; };
       };
-      # devices = {
-      # "device1" = { id = "DEVICE-ID-GOES-HERE"; };
-      # };
       folders = {
         "Music" = {
           path = "/home/wh1le/Music";
-          # devices = [ "device1" "device2" ];
+          devices = [ "thinkpad" ];
+        };
+        "Stereo" = {
+          path = "/home/wh1le/Stereo";
+          devices = [ "thinkpad" ];
         };
         "Documents" = {
           path = "/home/wh1le/Documents";
-          # devices = [ "device1" "device2" ];
+          devices = [ "thinkpad" ];
         };
-        # "Example" = {
-        #   path = "/home/myusername/Example";
-        #   devices = [ "device1" ];
-        #   ignorePerms = false; # Enable file permission syncing
-        # };
+        options = {
+          urAccepted = -1;
+        };
       };
 
       openDefaultPorts = true;
