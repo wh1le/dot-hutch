48816deb4bdcd89d4ce5eb707daead4382aa9fdf
Nikita M <nmiloserdov@proton.me>
2025-12-21 00:05:30 +0000

    remove obsidian and tmux

--- home/.config/zsh/modules/tmux.zsh
+++ home/.config/zsh/modules/tmux.zsh
@@ -1,5 +1,5 @@
 $HOME/.local/bin/public/tmux/nix-session
 $HOME/.local/bin/public/tmux/local-session
-$HOME/.local/bin/public/tmux/obsidian-session
-$HOME/.local/bin/public/tmux/todo-session
+# $HOME/.local/bin/public/tmux/obsidian-session
+# $HOME/.local/bin/public/tmux/todo-session
 $HOME/.local/bin/private/tmux/openvpn-session
