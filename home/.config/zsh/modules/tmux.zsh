1c3015c5beec0b530ee7c4bfc682eee782fd73e7
Nikita M. <nmiloserdov@proton.me>
2026-01-06 12:44:21 +0000

    do not use scripts from private namespace

--- home/.config/zsh/modules/tmux.zsh
+++ home/.config/zsh/modules/tmux.zsh
@@ -2,4 +2,4 @@ $HOME/.local/bin/public/tmux/nix-session
 $HOME/.local/bin/public/tmux/local-session
 # $HOME/.local/bin/public/tmux/obsidian-session
 # $HOME/.local/bin/public/tmux/todo-session
-$HOME/.local/bin/private/tmux/openvpn-session
+# $HOME/.local/bin/private/tmux/openvpn-session
