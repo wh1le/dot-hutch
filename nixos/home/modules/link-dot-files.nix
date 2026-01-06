d739ffd9c1194d3aba8d3d6269ba12bf2923a407
Nikita Miloserdov <nmiloserdov09@gmail.com>
2026-01-06 12:18:42 +0000

    link scripts

--- nixos/home/modules/link-dot-files.nix
+++ nixos/home/modules/link-dot-files.nix
@@ -24,10 +24,14 @@ in
     ${pkgs.bash}/bin/bash ${self}/scripts/linking/deploy-xdg-config.sh "${userHome}/.config" "${dotPublic}/home/.config"
   '';
 
+  home.activation.linkScripts = config.lib.dag.entryAfter [ "writeBoundary" ] ''
+    mkdir -p ${userHome}/.local/bin
+
+    ln -sfn ${dotPublic}/home/.local/bin/public ${userHome}/.local/bin/public
+    ln -sfn ${dotPublic}/home/.local/share/darkman ${userHome}/.local/share/darkman
+  '';
+
   home.activation.cloneSubmodules = config.lib.dag.entryAfter [ "writeBoundary" ] ''
     ${pkgs.git}/bin/git -C ${dotPublic} submodule update --init --recursive
   '';
-
-  # home.file.".local/bin/public".source = mkSymlink "${dotPublic}/home/.local/bin/public";
-  # home.file.".local/share/darkman".source = mkSymlink "${dotPublic}/home/.local/share/darkman";
 }
