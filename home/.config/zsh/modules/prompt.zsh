d36394d6f3745ee66b2a0c6b360d22be0842c4ca
wh1le <nmiloserdov@proton.me>
2026-01-10 10:52:51 +0000

    use new zsh prompt format: user@host:path

--- home/.config/zsh/modules/prompt.zsh
+++ home/.config/zsh/modules/prompt.zsh
@@ -1,5 +1,4 @@
 source ~/.cache/wal/colors.sh
 
 NEWLINE=$'\n'
-# PROMPT="${NEWLINE}%K{$color5}%F{$background}%n@%~ %f%k ❯ "
-PROMPT="${NEWLINE}%K{5}%F{0}%n@%~ %f%k ❯ "
+PROMPT="${NEWLINE}%K{5}%F{0}%n@%m%~ %f%k ❯ "
