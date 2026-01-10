f248cb6c0db61bc0cae392f210026eaed1f070b4
wh1le <nmiloserdov@proton.me>
2026-01-10 16:59:48 +0000

    add reddit search engine

--- home/.config/qutebrowser/modules/search_engines.py
+++ home/.config/qutebrowser/modules/search_engines.py
@@ -1,12 +1,13 @@
 def apply_search_engines(c, config):
     c.url.searchengines = {
         'DEFAULT': 'https://duckduckgo.com/?q={}',
-        '!d': 'https://duckduckgo.com/?q={}',
-        '!g': 'https://www.google.com/search?q={}',
+        '!d':  'https://duckduckgo.com/?q={}',
+        '!g':  'https://www.google.com/search?q={}',
         '!aw': 'https://wiki.archlinux.org/?search={}',
         '!gh': 'https://github.com/search?o=desc&q={}&s=stars',
         '!yt': 'https://www.youtube.com/results?search_query={}',
         '!cl': 'https://claude.ai/new?q={}',
+        '!r':  'https://www.reddit.com/search/?q={}',
 
         # nix
         '!np': 'https://search.nixos.org/packages?query={}',
