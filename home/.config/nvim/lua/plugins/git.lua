0b0b718a8bef52e3ef741d3eee7539b5f66c48e5
Nikita M. <nmiloserdov@proton.me>
2026-08-16 10:20:38 +0100

    consolidate git plugins

--- home/.config/nvim/lua/plugins/git.lua
+++ home/.config/nvim/lua/plugins/git.lua
@@ -1,26 +1,59 @@
 return {
 	{
-		"tpope/vim-fugitive",
-		init = function()
-			-- Diff* highlight colors moved to config/colors.lua
+		{
+			"tpope/vim-fugitive",
+			init = function()
+				-- Diff* highlight colors moved to config/colors.lua
 
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = "fugitive",
-				callback = function(ev)
-					vim.keymap.set("n", "<CR>", "gf", { buffer = ev.buf, desc = "Open file in current window" })
-				end,
-			})
-		end,
-		keys = {
-			{ "<leader>gs", "<cmd>Git<cr>", desc = "Git Status" },
-			{ "<leader>gd", "<cmd>Gdiffsplit<cr>", desc = "Git Diff" },
-			{ "<leader>gb", "<cmd>Git blame<cr>", desc = "Git Blame" },
+				vim.api.nvim_create_autocmd("FileType", {
+					pattern = "fugitive",
+					callback = function(ev)
+						vim.keymap.set("n", "<CR>", "gf", { buffer = ev.buf, desc = "Open file in current window" })
+					end,
+				})
+			end,
+			keys = {
+				{ "<leader>gs", "<cmd>Git<cr>", desc = "Git Status" },
+				{ "<leader>gd", "<cmd>Gdiffsplit<cr>", desc = "Git Diff" },
+				{ "<leader>gb", "<cmd>Git blame<cr>", desc = "Git Blame" },
+			},
+		},
+		{
+			"sindrets/diffview.nvim",
+			opts = {
+				use_icons = false,
+			},
 		},
 	},
 	{
-		"sindrets/diffview.nvim",
+		"isakbm/gitgraph.nvim",
 		opts = {
-			use_icons = false,
+			git_cmd = "git",
+			symbols = {
+				merge_commit = "M",
+				commit = "*",
+			},
+			format = {
+				timestamp = "%H:%M:%S %d-%m-%Y",
+				fields = { "hash", "timestamp", "author", "branch_name", "tag" },
+			},
+			hooks = {
+				on_select_commit = function(commit)
+					print("selected commit:", commit.hash)
+				end,
+				on_select_range_commit = function(from, to)
+					print("selected range:", from.hash, to.hash)
+				end,
+			},
+		},
+		keys = {
+			{
+				"<leader>gl",
+				function()
+					require("gitgraph").draw({}, { all = true, max_count = 5000 })
+				end,
+				desc = "GitGraph - Draw",
+			},
 		},
 	},
 }
