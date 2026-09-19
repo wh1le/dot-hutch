a38718c364da0c2c980972007633b79e339fc164
Nikita Miloserdov <nmiloserdov@proton.me>
2026-09-19 09:59:44 +0100

    nvim: disable nix lsp, oil, do not fllow symlink path

--- home/.config/nvim/lua/config/lsp/nix.lua
+++ home/.config/nvim/lua/config/lsp/nix.lua
@@ -1,20 +1,20 @@
-vim.lsp.config.nix = {
-	cmd = { "nil" },
-	filetypes = { "nix" },
-	root_markers = { "flake.nix" },
-	settings = {
-		["nil"] = {
-			formatting = {
-				command = { "nixfmt" },
-			},
-			nix = {
-				flake = {
-					autoArchive = false,
-					autoEvalInputs = true,
-				},
-			},
-		},
-	},
-}
-
-vim.lsp.enable("nix")
+-- vim.lsp.config.nix = {
+-- 	cmd = { "nil" },
+-- 	filetypes = { "nix" },
+-- 	root_markers = { "flake.nix" },
+-- 	settings = {
+-- 		["nil"] = {
+-- 			formatting = {
+-- 				command = { "nixfmt" },
+-- 			},
+-- 			nix = {
+-- 				flake = {
+-- 					autoArchive = false,
+-- 					autoEvalInputs = true,
+-- 				},
+-- 			},
+-- 		},
+-- 	},
+-- }
+--
+-- vim.lsp.enable("nix")
--- home/.config/nvim/lua/plugins/oil.lua
+++ home/.config/nvim/lua/plugins/oil.lua
@@ -1,3 +1,36 @@
+-- Keep symlink paths instead of resolving them to their target.
+local function no_symlink_resolve()
+	local files = require("oil.adapters.files")
+	local fs = require("oil.fs")
+	local util = require("oil.util")
+	local uv = vim.uv or vim.loop
+	files.normalize_url = function(url, callback)
+		local scheme, path = util.parse_url(url)
+		assert(path)
+		local os_path = vim.fn.fnamemodify(fs.posix_to_os_path(path), ":p")
+		uv.fs_stat(
+			os_path,
+			vim.schedule_wrap(function(_, stat)
+				local is_directory
+				if stat then
+					is_directory = stat.type == "directory"
+				elseif vim.endswith(os_path, "/") or (fs.is_windows and vim.endswith(os_path, "\\")) then
+					is_directory = true
+				else
+					local filetype = vim.filetype.match({ filename = vim.fs.basename(os_path) })
+					is_directory = filetype == nil
+				end
+
+				if is_directory then
+					callback(scheme .. util.addslash(fs.os_to_posix_path(os_path)))
+				else
+					callback(vim.fn.fnamemodify(os_path, ":."))
+				end
+			end)
+		)
+	end
+end
+
 return {
 	"stevearc/oil.nvim",
 	opts = {
@@ -10,6 +43,10 @@ return {
 			["<CR>"] = { "actions.select", mode = "n" },
 		},
 	},
+	config = function(_, opts)
+		require("oil").setup(opts)
+		no_symlink_resolve()
+	end,
 	lazy = false,
 	keys = {
 		{
